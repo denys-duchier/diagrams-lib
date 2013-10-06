436a39173263a7ff8ac1363cffd22a3af1b6949e
Daniel Bergey <bergey@alum.mit.edu>
2013-10-06 19:05:15 +0000

    Add Box 3D solid type

--- src/Diagrams/ThreeD/Shapes.hs
+++ src/Diagrams/ThreeD/Shapes.hs
@@ -21,6 +21,7 @@ module Diagrams.ThreeD.Shapes
        ) where
 
 import Prelude hiding (minimum)
+import Control.Newtype
 import Data.Semigroup
 
 import Data.AffineSpace
@@ -57,3 +58,34 @@ sphere = mkQD (Prim $ Ellipsoid mempty)
                 c = p' <.> p' - 1
                 p' = p .-. origin
         sphereQuery v = Any $ magnitudeSq (v .-. origin) <= 1
+
+data Box = Box T3
+
+type instance V Box = R3
+
+instance Transformable Box where
+    transform t1 (Box t2) = Box (t1 <> t2)
+
+instance IsPrim Box
+
+instance Renderable Box NullBackend where
+    render _ _ = mempty
+
+cube :: (Backend b R3, Renderable Box b) => Diagram b R3
+cube = mkQD (Prim $ Box mempty)
+            (mkEnvelope boxEnv)
+            (mkTrace boxTrace)
+            mempty
+            (Query boxQuery)
+  where
+    corners = [(0,0,0), (0,0,1), (0,1,0), (0,1,1),
+               (1,0,0), (1,0,1), (1,1,0), (1,1,1)]
+    boxEnv v = (maximum $ map ((normalized v <.>) . r3) corners) / magnitude v
+    boxTrace p v = minimum . map (magnitude . unpack)  . filter range . map atT $ ts where
+      (x0, y0, z0) = unp3 p
+      (vx, vy, vz) = unr3 v
+      ts = [-x0/vx, (1-x0)/vx, -y0/vy, (1-y0)/vy, -z0/vz, (1-z0)/vz]
+      atT t = p .+^ (t*^v)
+    range u = and [x >= 0, x <= 1, y >= 0, y >= 1, z <= 0, z >= 1] where
+      (x, y, z) = unp3 u
+    boxQuery = Any . range
